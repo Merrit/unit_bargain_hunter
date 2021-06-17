5f48a611b27b0a918879c1c06a6936c8134d01e3
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-06-17 11:26:25 -0400

    Add comment for PlatformRepository

--- lib/infrastructure/platform_repository/src/platform.dart
+++ lib/infrastructure/platform_repository/src/platform.dart
@@ -2,6 +2,9 @@ import 'platform_stub.dart'
     if (dart.library.html) 'platform_web.dart'
     if (dart.library.io) 'platform_general.dart';
 
+// Conditional imports since Flutter web cannot import dart:io,
+// and other Flutter versions cannot import dart:html without crashing.
+
 enum PlatformType {
   linux,
   macos,
