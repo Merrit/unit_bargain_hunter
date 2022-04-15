746ab4c22e32e45983759e032ed3c72880007cb6
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-04-15 12:24:54 -0400

    Set initial window size in platform code

--- lib/window/window.dart
+++ lib/window/window.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 import 'package:window_size/window_size.dart' as window_size;
 
@@ -15,34 +12,13 @@ class Window {
   Window._();
 
   static Future<Window> initialize() async {
+    _log.info('Initializing desktop application window.');
     instance = Window._();
     instance.setWindowTitle('Unit Bargain Hunter');
-    await instance.setWindowFrame(width: 635, height: 650);
     return instance;
   }
 
   static late final Window instance;
 
-  Future<Offset> _getScreenCenter() async {
-    final screen = await window_size.getCurrentScreen();
-    final screenRect = screen?.visibleFrame;
-    final position = screenRect?.center ?? Offset.zero;
-    if (position == Offset.zero) _log.info('Unable to find screen center.');
-    return position;
-  }
-
   void setWindowTitle(String title) => window_size.setWindowTitle(title);
-
-  /// Defaults to centering the window, however a check
-  /// could be added to customize the initial window position.
-  Future<void> setWindowFrame({
-    required double width,
-    required double height,
-  }) async {
-    if (kDebugMode) return; // Allow us more freedom when coding & testing.
-    final position = await _getScreenCenter();
-    window_size.setWindowFrame(
-      Rect.fromCenter(center: position, width: 635, height: 650),
-    );
-  }
 }
