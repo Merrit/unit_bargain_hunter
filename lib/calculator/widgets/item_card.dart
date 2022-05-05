fd4aa096fae31bf9e680bad2c4e986493a2e2b6a
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-05-05 10:35:16 -0400

    Improve item name text input

--- lib/calculator/widgets/item_card.dart
+++ lib/calculator/widgets/item_card.dart
@@ -190,50 +190,59 @@ class _ItemNameWidgetState extends State<ItemNameWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Focus(
-      focusNode: nameFocusNode,
-      onFocusChange: (focused) => setState(() => nameHasFocus = focused),
-      skipTraversal: true,
-      descendantsAreTraversable: false,
-      child: Consumer(
-        builder: (context, ref, child) {
-          final item = ref.watch(_currentItem);
+    return Consumer(
+      builder: (context, ref, child) {
+        final item = ref.watch(_currentItem);
 
-          Widget child;
-          if (nameFocusNode.hasFocus) {
-            child = TextFormField(
-              focusNode: textFieldFocusNode,
-              controller: controller,
-              autofocus: true,
-              textAlign: TextAlign.center,
-              textAlignVertical: TextAlignVertical.center,
-              onFieldSubmitted: (_) => _updateItem(item),
-              decoration: InputDecoration(
-                border: const UnderlineInputBorder(),
-                suffixIcon: IconButton(
-                  onPressed: () => _updateItem(item),
-                  padding: const EdgeInsets.all(4),
-                  icon: const Icon(Icons.done, color: Colors.green),
-                ),
-              ),
-            );
-          } else {
-            child = Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(text: '${item.name} '),
-                    const WidgetSpan(
-                      child: Icon(Icons.edit, size: 15, color: Colors.grey),
+        Widget child;
+        if (nameFocusNode.hasFocus) {
+          child = TextFormField(
+            focusNode: textFieldFocusNode,
+            controller: controller..selectAll(),
+            autofocus: true,
+            textAlign: TextAlign.center,
+            textAlignVertical: TextAlignVertical.center,
+            textCapitalization: TextCapitalization.words,
+            onFieldSubmitted: (_) => _updateItem(item),
+            decoration: InputDecoration(
+              border: const UnderlineInputBorder(),
+              suffixIcon: (isHandset(context))
+                  ? null
+                  : IconButton(
+                      onPressed: () => _updateItem(item),
+                      padding: const EdgeInsets.all(4),
+                      icon: const Icon(Icons.done, color: Colors.green),
                     ),
-                  ],
-                ),
+            ),
+          );
+        } else {
+          child = Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(text: '${item.name} '),
+                  const WidgetSpan(
+                    child: Icon(Icons.edit, size: 15, color: Colors.grey),
+                  ),
+                ],
               ),
-            );
-          }
+            ),
+          );
+        }
 
-          return Row(
+        return Focus(
+          focusNode: nameFocusNode,
+          onFocusChange: (focused) {
+            setState(() => nameHasFocus = focused);
+
+            if (focused) controller.text = item.name;
+
+            if (!focused) FocusManager.instance.primaryFocus?.unfocus();
+          },
+          skipTraversal: true,
+          descendantsAreTraversable: false,
+          child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               Expanded(
@@ -248,9 +257,9 @@ class _ItemNameWidgetState extends State<ItemNameWidget> {
                 ),
               ),
             ],
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
