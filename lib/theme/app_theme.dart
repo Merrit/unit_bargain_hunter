ce0337c3b1fcab46153197a685746fbb6a6cb8d7
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-06-07 14:19:17 -0400

    Switch font to Montserrat
    Much more legible and pleasant

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -24,6 +24,7 @@ class AppTheme {
     return ThemeData(
       brightness: brightness,
       cardTheme: cardTheme,
+      fontFamily: 'Montserrat',
       inputDecorationTheme: inputDecorationTheme,
     );
   }
