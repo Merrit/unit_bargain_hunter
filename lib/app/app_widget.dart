daef8bfc38d2ef0721c98ed1b181f61585f2605b
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-23 15:02:48 -0500

    feat: wait for sync before exiting app on desktop

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:window_manager/window_manager.dart';
 
+import '../calculator/calculator_cubit/calculator_cubit.dart';
 import '../calculator/calculator_page.dart';
+import '../logs/logs.dart';
 import '../purchases/pages/purchase_successful_page.dart';
 import '../purchases/pages/purchases_page.dart';
 import '../settings/settings.dart';
@@ -10,8 +13,48 @@ import '../shortcuts/app_shortcuts.dart';
 
 export 'cubit/app_cubit.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> with WindowListener {
+  @override
+  void initState() {
+    super.initState();
+    WindowManager.instance.addListener(this);
+  }
+
+  @override
+  void dispose() {
+    WindowManager.instance.removeListener(this);
+    super.dispose();
+  }
+
+  /// Whether the application is currently exiting.
+  ///
+  /// This is used to prevent the application from running the exit logic
+  /// multiple times while the window is closing.
+  bool _exitingApp = false;
+
+  /// Called when the window is closed.
+  ///
+  /// This is only called on desktop platforms.
+  ///
+  /// On desktop platforms, the window is hidden and the data is synced
+  /// before the application is exited.
+  @override
+  Future<void> onWindowClose() async {
+    if (_exitingApp) return;
+    _exitingApp = true;
+    log.v('Caught window close event. Hiding window and syncing data.');
+    await windowManager.hide();
+    await calcCubit.syncData();
+    log.v('Sync complete. Exiting application.');
+    await windowManager.destroy();
+  }
 
   @override
   Widget build(BuildContext context) {
